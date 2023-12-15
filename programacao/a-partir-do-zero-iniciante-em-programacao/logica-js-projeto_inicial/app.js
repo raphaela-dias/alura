c0ce51e3d783ab8546a843197ff8ee9b98a76876
RLMD <raphaelalmd@gmail.com>
2023-12-15 11:16:26 -0300

    Acidionada a variável chute e condicional para caso o chute seja acertado

--- programacao/a-partir-do-zero-iniciante-em-programacao/logica-js-projeto_inicial/app.js
+++ programacao/a-partir-do-zero-iniciante-em-programacao/logica-js-projeto_inicial/app.js
@@ -1,3 +1,7 @@
 alert("Bem vindo ao jogo do número secreto!");
 let numero_secreto = 29;
-prompt("Escolha um número entre 1 e 30")
+let chute = prompt("Escolha um número entre 1 e 30");
+
+if (chute == numero_secreto){
+    console.log("Você acertou o número secreto!");
+};
