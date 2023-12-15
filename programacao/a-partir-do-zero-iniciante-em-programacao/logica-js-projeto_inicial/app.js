ee7119a2fb330b410f0e9b0d6c10e3fc4ada8ef4
RLMD <147218285+RLMD@users.noreply.github.com>
2023-12-15 18:59:31 -0300

    Deleted programacao/a-partir-do-zero-iniciante-em-programacao/logica-js-projeto_inicial directory

--- programacao/a-partir-do-zero-iniciante-em-programacao/logica-js-projeto_inicial/app.js
+++
@@ -1,7 +0,0 @@
-alert("Bem vindo ao jogo do número secreto!");
-let numero_secreto = 29;
-let chute = prompt("Escolha um número entre 1 e 30");
-
-if (chute == numero_secreto){
-    console.log("Você acertou o número secreto!");
-};
