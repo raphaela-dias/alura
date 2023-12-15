802121dbcd0b31303d92e4f4ffc939547eb99452
RLMD <raphaelalmd@gmail.com>
2023-12-15 20:41:40 -0300

    Criado arquivo para o curso de Python com Orientação a Objetos

---
+++ programação/aprenda-a-programar-em-python-com-orientação-a-objetos/sabor-express/app.py
@@ -0,0 +1,11 @@
+print("""☆:.｡.o(≧▽≦)o.｡.:☆
+Sabor Express     
+☆:.｡.o(≧▽≦)o.｡.:☆
+      
+1. Cadastrar restaurante
+2. Listar restaurante
+3. Ativar restaurante
+4. Sair\n""")
+
+opcao_escolhida = input("Escolha uma opção: ")
+print(f"Você escolheu a opção {opcao_escolhida}.")
