7dcb5b4bbcd67489c48d3ef1446171080c2c2a0a
RLMD <raphaelalmd@gmail.com>
2023-12-19 13:06:11 -0300

    Adicionadas condicionais if/else para cada opção do aplicativo

--- programação/aprenda-a-programar-em-python-com-orientação-a-objetos/sabor-express/app.py
+++ programação/aprenda-a-programar-em-python-com-orientação-a-objetos/sabor-express/app.py
@@ -3,9 +3,17 @@ Sabor Express
 ☆:.｡.o(≧▽≦)o.｡.:☆
       
 1. Cadastrar restaurante
-2. Listar restaurante
+2. Listar restaurantes
 3. Ativar restaurante
 4. Sair\n""")
 
 opcao_escolhida = input("Escolha uma opção: ")
-print(f"Você escolheu a opção {opcao_escolhida}.")
+
+if opcao_escolhida == 1:
+    print("Cadastrando restaurante")
+elif opcao_escolhida == 2:
+    print("Listando restaurantes")
+elif opcao_escolhida == 3:
+    print("Ativando restaurante")
+else:
+    print("Encerrando o programa")
