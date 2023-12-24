0979485d2730d73e62002b83cc77d3085f43f160
RLMD <raphaelalmd@gmail.com>
2023-12-24 00:41:52 -0300

    Modularização do programa Divisão do programa em módulos para tornar a manutenção do mesmo mais fácil

--- programação/aprenda-a-programar-em-python-com-orientação-a-objetos/sabor-express/app.py
+++ programação/aprenda-a-programar-em-python-com-orientação-a-objetos/sabor-express/app.py
@@ -7,19 +7,19 @@ def finalizar_programa():
     # os.system('clear') para Mac
     print("Encerrando o programa!")
 
-#Programa principal
-if __name__ == '__main__':
+def exibir_nome_programa():
     print("""☆:.｡.o(≧▽≦)o.｡.:☆
-    Sabor Express     
-    ☆:.｡.o(≧▽≦)o.｡.:☆
-        
-    1. Cadastrar restaurante
+Sabor Express     
+☆:.｡.o(≧▽≦)o.｡.:☆\n""")
+
+def exibir_opcoes():
+    print("""    1. Cadastrar restaurante
     2. Listar restaurantes
     3. Ativar restaurante
     4. Sair\n""")
 
+def escolher_opcao():
     opcao_escolhida = int(input("Escolha uma opção: "))
-
     if opcao_escolhida == 1:
         print("Cadastrando restaurante")
     elif opcao_escolhida == 2:
@@ -28,3 +28,14 @@ if __name__ == '__main__':
         print("Ativando restaurante")
     else:
         finalizar_programa()
+    
+
+#Programa principal
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+if __name__ == '__main__':
+    main()
