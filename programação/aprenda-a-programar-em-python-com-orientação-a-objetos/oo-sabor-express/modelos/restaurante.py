87473fb4456088fc4727875326d9ae5c362047ea
RLMD <raphaelalmd@gmail.com>
2024-02-14 02:13:56 -0300

    Adicionados valores em alguns atributos na classe Restaurante para teste

--- programação/aprenda-a-programar-em-python-com-orientação-a-objetos/oo-sabor-express/modelos/restaurante.py
+++ programação/aprenda-a-programar-em-python-com-orientação-a-objetos/oo-sabor-express/modelos/restaurante.py
@@ -4,8 +4,17 @@ class Restaurante:
     ativo = False
 
 restaurante_praca = Restaurante()
+restaurante_praca.nome = 'Praça'
+restaurante_praca.categoria = 'Gourmet'
 restaurante_pizza = Restaurante()
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurantes)
+#dir() - mostra todas as classes do objeto
+#print(dir(restaurante_praca))
+
+#vars() - mostra um dicionário com os atributos da classe do objeto
+#print(vars(restaurante_praca))
+
+#mostra o valor do atributo
+print(restaurante_praca.ativo)
