5d7d8dc73a930e389ec958cce4724e1def837763
RLMD <raphaelalmd@gmail.com>
2024-02-17 15:03:39 -0300

    Criado método para listar os restaurantes cadastrados

--- programação/aprenda-a-programar-em-python-com-orientação-a-objetos/oo-sabor-express/modelos/restaurante.py
+++ programação/aprenda-a-programar-em-python-com-orientação-a-objetos/oo-sabor-express/modelos/restaurante.py
@@ -1,16 +1,24 @@
 class Restaurante:
+
+    restaurantes = []
+
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurante():
+        for restaurante in Restaurante.restaurantes:
+            print(f"{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}")
 
 restaurante_praca = Restaurante('Praça', 'Goumert')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+Restaurante.listar_restaurante()
 
 #dir() - mostra todas as classes do objeto
 #print(dir(restaurante_praca))
@@ -20,9 +28,3 @@ restaurantes = [restaurante_praca, restaurante_pizza]
 
 #mostra o valor do atributo
 #print(restaurante_praca.ativo)
-
-print(restaurante_praca)
-print(restaurante_pizza)
-
-#print(vars(restaurante_praca))
-#print(vars(restaurante_pizza))
