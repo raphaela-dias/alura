601b059102f2c53411081416e78807ecea5ef850
RLMD <raphaelalmd@gmail.com>
2024-02-22 17:13:48 -0300

    Adicionado método para alterar o status do restaurante

--- programação/aprenda-a-programar-em-python-com-orientação-a-objetos/oo-sabor-express/modelos/restaurante.py
+++ programação/aprenda-a-programar-em-python-com-orientação-a-objetos/oo-sabor-express/modelos/restaurante.py
@@ -10,19 +10,23 @@ class Restaurante:
 
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
-    
-    def listar_restaurante():
+
+    @classmethod    
+    def listar_restaurante(cls):
         print(f"{'Nome do restaurante.'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'} ")
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f"{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}")
 
     @property
     def ativo(self):
-        return '✓' if self._ativo else '✗'
+       return '✓' if self._ativo else '✗'
+
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('praça', 'Goumert')
 restaurante_pizza = Restaurante('pizza express', 'Italiana')
-restaurante_praca._nome = 'Praça 2.0'
+restaurante_praca.alternar_estado()
 Restaurante.listar_restaurante()
 
 #dir() - mostra todas as classes do objeto
