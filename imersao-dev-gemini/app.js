f9b4028eed8604a97789440f84f0b4252fd708eb
RLMD <raphaelalmd@gmail.com>
2024-09-07 19:33:10 -0300

    Aula 5 - Finalizando o projeto

--- imersao-dev-gemini/app.js
+++ imersao-dev-gemini/app.js
@@ -2,21 +2,43 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
     // Inicializa uma string vazia para armazenar os resultados HTML
     let resultados = '';
 
-    // Itera sobre cada item de dados (assumindo que 'dados' é um array de objetos)
-    for (let dado of dados) {
-        // Concatena o HTML de cada resultado à string 'resultados'
-        resultados += `
-            <div class="item-resultado"> 
-                <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
-                <p class="descricao-meta">${dado.descricao}</p>
-                <a href=${dado.link} target="_blank">Mais informações</a>
-            </div>
-        `;
+    if (campoPesquisa == ''){
+        section.innerHTML = "<p>Nada foi encontrado</p>"
+        return
     }
 
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let titulo = '';
+    let descricao = '';
+    let tags = '';
+
+    // Itera sobre cada item de dados (assumindo que 'dados' é um array de objetos)
+    for (let dado of dados) {
+        titulo = dado.titulo.toLocaleLowerCase();
+        descricao = dado.descricao.toLocaleLowerCase();
+        tags = dado.tags.toLocaleLowerCase();
+
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            // Concatena o HTML de cada resultado à string 'resultados'
+            resultados += `
+                <div class="item-resultado"> 
+                    <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href=${dado.link} target="_blank">Mais informações</a>
+                </div>
+            `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado<p>";
+    }
     // Atribui o HTML completo da lista de resultados à seção
     section.innerHTML = resultados;
 }
--- imersao-dev-gemini/dados.js
+++ imersao-dev-gemini/dados.js
@@ -16,18 +16,21 @@ let rayssa = {
 let dados = [{
     titulo: "Beatriz Souza",
     descricao: "Beatriz Souza é uma judoca brasileira, campeã olímpica e sargento do exército. Com sua força e técnica, ela conquistou o topo do pódio e se tornou um orgulho para o Brasil.",
-    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo"
     },
 
     {
     titulo: "Rayssa Leal",
     descricao: "Rayssa Andrade é uma skatista profissional brasileira, conhecida por sua habilidade e estilo únicos. Ela conquistou diversos títulos e se tornou um ícone do skate feminino no Brasil e no mundo.",
-    link: "https://en.wikipedia.org/wiki/Rayssa_Leal"
+    link: "https://en.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada"
     },
 
     {
     titulo: "Rebeca Andrade",
     descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira e mundial. Com sua força, agilidade e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte.",
-    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginasta ginastica"
     }
 ];
